13b3f0873efd6061a030db5c2c7f93883b0b6b9a
james shao <jshao100@gmail.com>
2017-08-15 22:06:21 -0400

    experience done, misc links added

--- experiment.php
+++ experiment.php
@@ -28,26 +28,146 @@
 			<div class="circle medium blue pos_about">
 				<a class="middle click_to_zoom">Me</a>
 			</div>
+			<div class="circle small orange pos_resume">
+				<a class="fa fa-file-text-o resume" aria-hidden="true"></a>
+			</div>
+			<div class="circle small orange pos_mailto">
+				<a class="fa fa-envelope-o mailto" aria-hidden="true"></a>
+			</div>
+			<div class="circle small orange pos_github">
+				<a class="fa fa-github github" aria-hidden="true"></a>
+			</div>
+			<div class="circle small orange pos_instagram">
+				<a class="fa fa-instagram instagram" aria-hidden="true"></a>
+			</div>
+			<div class="circle small orange pos_linkedin">
+				<a class="fa fa-linkedin linkedin" aria-hidden="true"></a>
+			</div>
 
 			<!-- PROJECTS -->
 			<div class="proj_content">
-				<div class="proj1">
-					<div class="circle small blue pos_proj1"><a class="middle x2">Project 1</a></div>
-					<div class="circle x-small red pos_proj1_1"></div>
-					<div class="circle x-small yellow pos_proj1_2"></div>
-					<div class="circle x-small blue pos_proj1_3"></div>
-					<div class="circle xx-small green pos_proj1_4"></div>
-				</div>
-				<div class="circle small red pos_proj2 show_on_zoom"></div>
-				<div class="circle small yellow pos_proj3 show_on_zoom"></div>
-				<div class="circle small green pos_proj4 show_on_zoom"></div>
+
+<?php 
+	/* FILE FORMAT
+	 *
+	 * Project Title
+	 * Date
+	 * Technology
+	 * Sentence 1
+	 * (optional) Sentence 2
+	 * Github Link
+	 *
+	 */
+
+error_reporting(E_ERROR | E_PARSE);
+$numProj = 4;
+
+for ($i = 1; $i <= $numProj; $i++) {
+	$handle = fopen("./text/projects/proj".$i.".txt", "r");
+	if ($handle) {
+		$title;
+		$date;
+		$tech;
+		$s1;
+		$s2 = false;
+		$git;
+
+		$count = 0;
+		while (($line = fgets($handle)) !== false) {
+			if ($count == 0) {
+				$title = $line;
+			} else if ($count == 1) {
+				$date = $line;
+			} else if ($count == 2) {
+				$tech = $line;
+			} else if ($count == 3) {
+				$s1 = $line;
+			} else if ($count == 4) {
+				$next = fgets($handle);
+
+				//next not null
+				if ($next !== false) {
+					$s2 = $line;
+					$git = $next;
+				} else {
+					$git = $line;
+				}
+			}
+			$count++;
+		}
+		fclose($handle);
+
+		echo '<div class="proj'.$i.'">';
+		echo '<div class="circle small blue pos_proj'.$i.'">';
+		echo '<a class="middle x2">'.$title.'<span>'.$date.'</span></a></div>';
+		echo '<div class="circle x-small red pos_proj'.$i.'_tech"><p>'.$tech.'</div>';
+		echo '<div class="circle x-small yellow pos_proj'.$i.'_s1"><p>'.$s1.'</div>';
+		if ($s2 !== false) {
+			echo '<div class="circle x-small orange pos_proj'.$i.'_s2"><p>'.$s2.'</div>';
+		}
+		echo '<div class="circle xx-small green pos_proj'.$i.'_git">';
+		echo '<a href="'.$git.'" class="fa fa-github" aria-hidden="true" target="_blank"></a></div>';
+		echo '</div>';
+	}
+}
+?>
 			</div>
 
 			<!-- EXPERIENCE -->
-			<div class="circle small blue pos_exp1 show_on_zoom"></div>
-			<div class="circle small red pos_exp2 show_on_zoom"></div>
-			<div class="circle small yellow pos_exp3 show_on_zoom"></div>
-			<div class="circle small green pos_exp4 show_on_zoom"></div>
+			<div class="exp_content">
+<?php 
+	/* FILE FORMAT
+	 *
+	 * Title
+	 * Date
+	 * S1
+	 * S2
+	 * S3
+	 *
+	 */
+
+error_reporting(E_ERROR | E_PARSE);
+$numProj = 4;
+
+for ($i = 1; $i <= $numProj; $i++) {
+	$handle = fopen("./text/experience/exp".$i.".txt", "r");
+	if ($handle) {
+		$title;
+		$date;
+		$s1;
+		$s2;
+		$s3;
+
+		$count = 0;
+		while (($line = fgets($handle)) !== false) {
+			if ($count == 0) {
+				$title = $line;
+			} else if ($count == 1) {
+				$date = $line;
+			} else if ($count == 2) {
+				$s1 = $line;
+			} else if ($count == 3) {
+				$s2 = $line;
+			} else if ($count == 4) {
+				$s3 = $line;
+			}
+			$count++;
+		}
+		fclose($handle);
+
+		echo '<div class="exp'.$i.'">';
+		echo '<div class="circle small yellow pos_exp'.$i.' show_on_zoom">';
+		echo '<a class="middle x2">'.$title.'<span>'.$date.'</span></a></div>';
+		echo '<div class="circle x-small blue pos_exp'.$i.'_1"><p>'.$s1.'</p></div>';
+		echo '<div class="circle x-small green pos_exp'.$i.'_2"><p>'.$s2.'</p></div>';
+		echo '<div class="circle x-small red pos_exp'.$i.'_3"><p>'.$s3.'</p></div>';
+		echo '</div>';
+
+	}
+
+}
+		?>
+			</div>
 
 		</div>
 		<script src="/bower_components/foundation/js/vendor/jquery.js"></script>
--- js/experiment.js
+++ js/experiment.js
@@ -1,36 +1,89 @@
 $( document ).ready(function() {
 	$(".reset").click(function() {
-		$(".experiment").removeClass("zoom");
-		$(".show").removeClass("show");
+		clean();
 		$(".experiment").css("transform-origin", "");
 	});
 
+	//main navigation
 	$(".click_to_zoom").click(function(){
 		//get parent
-		$(".experiment").removeClass("zoom2");
-		$(".show").removeClass("show");
+		clean();
+		$(".experiment").addClass("zoom");
+
 		if ($(this).parent().hasClass("pos_projects")) {
-			$(".experiment").addClass("zoom");
 			$(".experiment").css("transform-origin","25vw 42vw");
+			$(".experiment").addClass("zoom_proj");
 
 			//add class show to all children
-			$(".proj_content").children().children().first().children().addClass("show");
+			$(".proj_content").children().children(".small").children("a").addClass("show");
 
 		} else if ($(this).parent().hasClass("pos_exp")) {
-			$(".experiment").addClass("zoom");
-			$(".experiment").css("transform-origin","84vw 8vw");
+			$(".experiment").css("transform-origin","84vw 4vw");
+			$(".experiment").addClass("zoom_exp");
+			
+			//add class show to all children
+			$(".exp_content").children().children(".small").children("a").addClass("show");
 		} else if ($(this).parent().hasClass("pos_about")) {
-			$(".experiment").addClass("zoom");
 			$(".experiment").css("transform-origin","90vw 51vw");
+			$(".experiment").addClass("zoom_about");
 		}
 	});
 
+	//2nd layer
 	$(".x2").click(function() {
-		if ($(".experiment").hasClass("zoom")) {
+		$(".showX").removeClass("showX");
+		if ($(".experiment").hasClass("zoom") || $(".experiment").hasClass("zoom2")) {
+		
 			//zoom in on specific project
-			$(".experiment").removeClass("zoom");
-			$(".experiment").addClass("zoom2");
-			$(".experiment").css("transform-origin","20vw 25vw");
+			if ($(".experiment").hasClass("zoom_proj")) {
+				$(".experiment").removeClass("zoom2 zoom_exp zoom_about");
+				$(".experiment").addClass("zoom2");
+
+				//zoom in on specific clicked element
+				if ($(this).parent().parent().hasClass("proj1")) {
+					$(".experiment").css("transform-origin","20vw 25vw");
+					$(".proj1").find(".x-small").children().addClass("showX");
+					$(".proj1").find(".xx-small").children().addClass("showX");
+				} else if ($(this).parent().parent().hasClass("proj2")) {
+					$(".experiment").css("transform-origin","11vw 35vw");
+					$(".proj2").find(".x-small").children().addClass("showX");
+					$(".proj2").find(".xx-small").children().addClass("showX");
+				} else if ($(this).parent().parent().hasClass("proj3")) {
+					$(".experiment").css("transform-origin","17vw 50.5vw");
+					$(".proj3").find(".x-small").children().addClass("showX");
+					$(".proj3").find(".xx-small").children().addClass("showX");
+				} else if ($(this).parent().parent().hasClass("proj4")) {
+					$(".experiment").css("transform-origin","47vw 46.5vw");
+					$(".proj4").find(".x-small").children().addClass("showX");
+					$(".proj4").find(".xx-small").children().addClass("showX");
+				}
+			}
+			
+			//zoom in on specific experience
+			if ($(".experiment").hasClass("zoom_exp")) {
+				$(".experiment").removeClass("zoom2 zoom_proj zoom_about");
+				$(".experiment").addClass("zoom2");
+
+				if ($(this).parent().parent().hasClass("exp1")) {
+					$(".experiment").css("transform-origin","60vw 0vw");
+					$(".exp1").find(".x-small").children().addClass("showX");
+				} else if ($(this).parent().parent().hasClass("exp2")) {
+					$(".experiment").css("transform-origin","87vw 0vw");
+					$(".exp2").find(".x-small").children().addClass("showX");
+				} else if ($(this).parent().parent().hasClass("exp3")) {
+					$(".experiment").css("transform-origin","87vw 15vw");
+					$(".exp3").find(".x-small").children().addClass("showX");
+				} else if ($(this).parent().parent().hasClass("exp4")) {
+					$(".experiment").css("transform-origin","84vw 28.5vw");
+					$(".exp4").find(".x-small").children().addClass("showX");
+				}
+			}
 		}
 	});
 });
+
+function clean() {
+	$(".experiment").removeClass("zoom zoom2 zoom_proj zoom_exp zoom_about");
+	$(".show").removeClass("show");
+	$(".showX").removeClass("showX");
+}
